1f157c83fe1d4434490035d967db3db8e47c6b6a
Cristopher <cristopherosc30@gmail.com>
2023-03-20 14:34:23 -0400

    Añadida la validacion de horas

--- Autohorario/Validacion.cs
+++ Autohorario/Validacion.cs
@@ -14,19 +14,43 @@ namespace Autohorario
 
         public static void Getdata(string codigo_asignatura, int id_seccion, int id_profesor, int creditos_asignatura, List<(string, int)> horario_seleccionado)
         {
+            bool verdad = Validarhoras(codigo_asignatura, id_seccion, creditos_asignatura, horario_seleccionado);
+        }
+        private static bool Validarhoras(string codigo_asignatura, int id_seccion, int creditos_asignatura,  List<(string, int)> horario_seleccionado)
+        {
             using (SqlCommand cmd = new SqlCommand("ppCheck_schedule", con))
             {
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.AddWithValue("@codigo_asignatura", codigo_asignatura);
+                cmd.Parameters.AddWithValue("@id_seccion", id_seccion);
+                List<(string, int)> horario_disponible = new List<(string, int)>();
 
                 using (SqlDataReader reader = cmd.ExecuteReader())
                 {
-                    while (reader.Read())
+                    for (int i = 0; i < horario_seleccionado.Count; i++)
                     {
-                        Console.WriteLine(reader.GetString(0));
+                        int hora_inicio_seleccion = int.Parse(horario_seleccionado[i].Item1.Substring(0,2));
+                        int hora_fin_seleccion = int.Parse(horario_seleccionado[i].Item1.Substring(3, 2));
+                        while (reader.Read())
+                        {
+                            int hora_inicio_asignatura = int.Parse(reader.GetString(0).Substring(0, 2));
+                            int hora_fin_asignatura = int.Parse(reader.GetString(0).Substring(3, 2));
+                            if (horario_seleccionado[i].Item2 == reader.GetInt32(1))
+                            {
+                                if (hora_inicio_asignatura == hora_inicio_seleccion && hora_fin_asignatura < hora_fin_seleccion)
+                                {
+                                    horario_disponible.Add(($"{hora_fin_asignatura}/{hora_fin_seleccion}", horario_seleccionado[i].Item2));
+                                } else if (hora_inicio_asignatura < hora_inicio_seleccion && hora_fin_asignatura == hora_fin_seleccion)
+                                {
+                                    horario_disponible.Add(($"{hora_inicio_seleccion}/{hora_fin_seleccion}", horario_seleccionado[i].Item2));
+                                }
+                            }
+                            
+                        }
                     }
                 }
             }
+            return true;
         }
     }
 }
