9df095beb85b0a0515c02d9b1b4437dcd4b9c984
Cristopher <cristopherosc30@gmail.com>
2023-03-20 14:50:39 -0400

    añadidas nuevas funcionalidades de validarhoras

--- Autohorario/Validacion.cs
+++ Autohorario/Validacion.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,13 @@ namespace Autohorario
 
         public static void Getdata(string codigo_asignatura, int id_seccion, int id_profesor, int creditos_asignatura, List<(string, int)> horario_seleccionado)
         {
-            bool verdad = Validarhoras(codigo_asignatura, id_seccion, creditos_asignatura, horario_seleccionado);
+            List<(string, int)> horario_disponible = Validarhoras(codigo_asignatura, id_seccion, horario_seleccionado);
+            foreach ((string, int) item in horario_disponible)
+            {
+                Console.WriteLine(item.Item1);
+            }
         }
-        private static bool Validarhoras(string codigo_asignatura, int id_seccion, int creditos_asignatura,  List<(string, int)> horario_seleccionado)
+        private static List<(string, int)> Validarhoras(string codigo_asignatura, int id_seccion,  List<(string, int)> horario_seleccionado)
         {
             using (SqlCommand cmd = new SqlCommand("ppCheck_schedule", con))
             {
@@ -35,22 +40,37 @@ namespace Autohorario
                         {
                             int hora_inicio_asignatura = int.Parse(reader.GetString(0).Substring(0, 2));
                             int hora_fin_asignatura = int.Parse(reader.GetString(0).Substring(3, 2));
+                            Console.WriteLine($"{hora_inicio_asignatura}, {hora_fin_asignatura}");
                             if (horario_seleccionado[i].Item2 == reader.GetInt32(1))
                             {
-                                if (hora_inicio_asignatura == hora_inicio_seleccion && hora_fin_asignatura < hora_fin_seleccion)
+                                if (hora_inicio_seleccion >= hora_inicio_asignatura && hora_fin_asignatura < hora_fin_seleccion)
                                 {
                                     horario_disponible.Add(($"{hora_fin_asignatura}/{hora_fin_seleccion}", horario_seleccionado[i].Item2));
-                                } else if (hora_inicio_asignatura < hora_inicio_seleccion && hora_fin_asignatura == hora_fin_seleccion)
+                                } 
+                                else if (hora_inicio_seleccion < hora_inicio_asignatura && hora_fin_asignatura >= hora_fin_seleccion)
                                 {
-                                    horario_disponible.Add(($"{hora_inicio_seleccion}/{hora_fin_seleccion}", horario_seleccionado[i].Item2));
+                                    horario_disponible.Add(($"{hora_inicio_seleccion}/{hora_inicio_asignatura}", horario_seleccionado[i].Item2));
+                                }
+                                else if (hora_inicio_seleccion < hora_inicio_asignatura && hora_fin_asignatura < hora_fin_seleccion)
+                                {
+                                    horario_disponible.Add(($"{hora_inicio_seleccion}/{hora_inicio_asignatura}", horario_seleccionado[i].Item2));
+                                    horario_disponible.Add(($"{hora_fin_asignatura}/{hora_fin_seleccion}", horario_seleccionado[i].Item2));
+                                }
+                                else
+                                {
+                                    horario_disponible.Add((horario_seleccionado[i].Item1, horario_seleccionado[i].Item2));
                                 }
                             }
                             
                         }
                     }
+                    if (horario_disponible.Count == 0)
+                    {
+                        horario_disponible = horario_seleccionado;
+                    }
+                    return horario_disponible;
                 }
             }
-            return true;
         }
     }
 }
