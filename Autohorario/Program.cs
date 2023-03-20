6b3bd6dcc1dd4f4876e36d9de265dda56affd5a5
Cristopher <cristopherosc30@gmail.com>
2023-03-19 22:20:06 -0400

    Agregar archivos de proyecto.

---
+++ Autohorario/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autohorario
+{
+    internal class Obtencion
+    {
+        internal static SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["horarioConexion"].ConnectionString);
+        static void Main(string[] args)
+        {
+            con.Open();
+            using (SqlCommand command = new SqlCommand("ppGetasignatura", con))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataSet ds = new DataSet();
+
+                adapter.Fill(ds);
+
+                foreach (DataRow item in ds.Tables[0].Rows)
+                {
+                    List<(string, int)> horario_seleccionado = gethorarios_seleccionado(int.Parse(item[2].ToString()));
+                    Validacion.Getdata(item[0].ToString(), int.Parse(item[1].ToString()), int.Parse(item[2].ToString()), int.Parse(item[3].ToString()), horario_seleccionado);
+                }
+            }
+        }
+        private static List<(string, int)> gethorarios_seleccionado(int id_profesor)
+        {
+            List<(string, int)> horario_seleccionado = new List<(string, int)>();
+
+            using (SqlCommand cmd = new SqlCommand("Gethorarios_seleccionado", con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id_profesor", id_profesor);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        horario_seleccionado.Add((reader.GetString(0), reader.GetInt32(1)));
+                    }
+                }
+            }
+            return horario_seleccionado;
+        }
+    }
+}
---
+++ Autohorario/Validacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autohorario
+{
+    internal class Validacion
+    {
+        SqlConnection con = Obtencion.con;
+
+        public static void Getdata(string codigo_asignatura, int id_seccion, int id_profesor, int creditos_asignatura, List<(string, int)> horario_seleccionado)
+        {
+             
+        }
+    }
+}
