e45d9a37156fc2b11522bdfc4e555a2881fd5ec1
Cristopher <cristopherosc30@gmail.com>
2023-03-19 23:23:00 -0400

    añadida la validacion para las horas dispnibles

--- Autohorario/Validacion.cs
+++ Autohorario/Validacion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,23 @@ namespace Autohorario
 {
     internal class Validacion
     {
-        SqlConnection con = Obtencion.con;
+        static SqlConnection con = Obtencion.con;
 
         public static void Getdata(string codigo_asignatura, int id_seccion, int id_profesor, int creditos_asignatura, List<(string, int)> horario_seleccionado)
         {
-             
+            using (SqlCommand cmd = new SqlCommand("ppCheck_schedule", con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@codigo_asignatura", codigo_asignatura);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader.GetString(0));
+                    }
+                }
+            }
         }
     }
 }
